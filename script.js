4ce7014a1c02c1bb4f5537ef6aa6ac0b6265fc09
Al <alpagar9@gmail.com>
2022-08-21 18:49:09 -0400

    workin on stuff

---
+++ script.js
@@ -0,0 +1,16 @@
+// Toggle grid padding
+function myFunction() {
+  var x = document.getElementById("myGrid");
+  window.alert("yuh");
+
+}
+
+// Open and close sidebar
+function w3_open() {
+  document.getElementById("mySidebar").style.width = "100%";
+  document.getElementById("mySidebar").style.display = "block";
+}
+
+function w3_close() {
+  document.getElementById("mySidebar").style.display = "none";
+}
